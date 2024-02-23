8d72e0a8bc479cc580e3b35fffddcf7fcf2c8766
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-23 19:49:50 +0200

    Added my project

---
+++ Week7-Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Are You coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+if (userChoice == "in")
+{ PrintHello(); }
+else
+{
+    PrintGoodbye();
+}
+
+static void PrintHello() //function /method
+{
+Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
